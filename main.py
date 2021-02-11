641dd6a4423375e2a0651eccebe9b68942a2cf52
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2021-02-11 18:03:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ sym = """
 \___ \| __/ _ \/ _` |/ _` | '_ \ / _ \___ \ / _` | | | | '__|
  ___) | ||  __/ (_| | (_| | | | | (_) |__) | (_| | |_| | |
 |____/ \__\___|\__, |\__,_|_| |_|\___/____/ \__,_|\__,_|_|
-               |___/     By RPD-512
+               |___/     By RPD-512 ( https://twitter.com/RhiddhiD )
 
 Press ctrl+c or type 'bye' to exit
 Type 'clear' to clear screen
